8a822e38b1a9eb0f10f768a54a49240298b23473
Maria Temponera <marakitempo@yahoo.gr>
2024-04-01 11:02:20 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   name = 'Maria';
-
-  person = {
-    givenName: 'Maria',
-    surName: 'Temponera',
-    age: 0x20,
-    email: 'mariatemp@aueb.gr'
-  }
 }
