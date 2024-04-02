b184488c47ca8c3d3ad4286b3ecb6652d7d9c164
Maria Temponera <marakitempo@yahoo.gr>
2024-04-02 19:43:22 +0300

    component output

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
 import { ComponetInputExampleComponent } from './components/componet-input-example/componet-input-example.component';
 import { ForDirectiveExampleComponent } from './components/for-directive-example/for-directive-example.component';
 import { SimpleDatatableExampleComponent } from './components/simple-datatable-example/simple-datatable-example.component';
+import { ComponentOutputExampleComponent } from './components/component-output-example/component-output-example.component';
 
 export const routes: Routes = [
   {path: 'for-directive-example', component: ForDirectiveExampleComponent},
   {path: 'component-input-example', component: ComponetInputExampleComponent},
   {path: 'simple-datatable-example', component: SimpleDatatableExampleComponent},
   {path: 'event-bind-example', component: EventBindExampleComponent},
+  {path: 'component-output-example', component: ComponentOutputExampleComponent},
   {path: '', component: WelcomeComponent},
 ];
