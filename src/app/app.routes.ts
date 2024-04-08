5936539d5b6e7c71fe94f8b692f9eff9c9783e15
Maria Temponera <marakitempo@yahoo.gr>
2024-04-08 19:46:54 +0300

    template driven forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ComponetInputExampleComponent } from './components/componet-input-examp
 import { ForDirectiveExampleComponent } from './components/for-directive-example/for-directive-example.component';
 import { SimpleDatatableExampleComponent } from './components/simple-datatable-example/simple-datatable-example.component';
 import { ComponentOutputExampleComponent } from './components/component-output-example/component-output-example.component';
+import { TemplateDrivenFormExampleComponent } from './components/template-driven-form-example/template-driven-form-example.component';
 
 export const routes: Routes = [
   {path: 'for-directive-example', component: ForDirectiveExampleComponent},
@@ -12,5 +13,6 @@ export const routes: Routes = [
   {path: 'simple-datatable-example', component: SimpleDatatableExampleComponent},
   {path: 'event-bind-example', component: EventBindExampleComponent},
   {path: 'component-output-example', component: ComponentOutputExampleComponent},
+  {path: 'template-driven-form-example', component: TemplateDrivenFormExampleComponent},
   {path: '', component: WelcomeComponent},
 ];
